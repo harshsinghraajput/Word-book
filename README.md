2137dfd0def3e1bfbc738cb89ccaaba66cd93a2f
Harsh Singh Rajput <iamharshsinghrajput@gmail.com>
2024-12-21 10:30:00 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Word-book
+ Python program which takes a user input and searches word and there meaning ...... Working just like a dictonories.
---
+++ wordbook.py
@@ -0,0 +1,37 @@
+import json
+from difflib import get_close_matches
+
+elements = json.load(open("wordbook.json"))
+
+def findmeaning(word):
+    if word.lower() in elements:
+        return elements[word.lower()]
+    elif word.upper() in elements:
+        return elements[word.upper]
+    elif word.title() in elements:
+        return elements[word.title()]
+    elif len(get_close_matches(word,elements.keys())) > 0:
+        closematches = (get_close_matches(word,elements.keys())[0])
+        user_decision = input("Are you looking for %s instead? [Y/N] " % closematches)
+
+        if user_decision == "Y":
+            return elements[get_close_matches(word,elements.keys())[0]]
+        if user_decision == "N":
+            return "I can't find your word, I'm sorry"
+        else:
+            return "Sorry! I cannot find your word"
+
+    else:
+        return "I can't fimd this word. Please look for spelling mistakes"
+
+word = input("Type Any Word Here: ")
+
+output = findmeaning(word)
+
+if type(output) == list:
+    for i in output:
+        print(i)
+else:
+    print(output)        
+
+        
